eff786d029859b19c6dc1cef34a0c7153124e901
kkm980 <kantkrishna751@gmail.com>
2024-11-14 19:35:00 +0530

    MLWEB-Logo:Added designed logo

--- components/navBar/logo/index.tsx
+++ components/navBar/logo/index.tsx
@@ -13,7 +13,7 @@ export default function Logo() {
             luggageRef.current,
             { x: 0, opacity: 0.7, rotation: 45 },  // Starting position with a slight tilt
             { 
-                x: 175, 
+                x: 196, 
                 opacity: 1, 
                 rotation: 0,  // Final position aligned at 0 degrees
                 duration: 5, 
@@ -25,7 +25,8 @@ export default function Logo() {
     return (
         <div className="flex items-center mr-8 cursor-pointer">
             <Luggage ref={luggageRef} className="h-8 w-8 text-primary" />
-            <span className="text-xl font-bold m-0 p-0">MeraLuggage</span>
+            <span className="text-2xl font-bold m-0 p-0 bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent
+      dark:from-indigo-400 dark:to-teal-400">MeraLuggage</span>
         </div>
     );
 }
